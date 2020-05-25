6b07fd76bd50d1bf742368d3070733e9ae35c6c8
Gonzalo Spina <gspina1995@gmail.com>
2020-05-25 18:07:16 -0300

    Finished with the screen fill

--- MachineLanguage/fill/Fill.asm
+++ MachineLanguage/fill/Fill.asm
@@ -1,14 +1,57 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+  @SCREEN // Initial position is left top pixel
+  D=A
+  @position
+  M=D-1
+
+(LOOP)
+  @KBD // Read keyboard
+  D=M
+  @BLACK
+  D;JGT // If D>0 go blacl
+  @WHITE
+  0;JMP // else go white
+
+(WHITE)
+  @SCREEN // If position is left top, screen is already white so we must jump to loop
+  D=A-1
+  @position
+  D=D-M
+  @LOOP
+  D;JEQ
+
+  // Else: We paint the screen with white pixels
+  @position
+  A=M
+  M=0
+
+  @position // Move position forward
+  M=M-1
+
+  @LOOP // Return to loop
+  0;JMP
+
+(BLACK)
+  @24576 // If position is right bottom, screen is already black so we must jump to loop
+  D=M
+  @position
+  D=D-M
+  @LOOP
+  D;JEQ
+
+  // Else: We paint the screen with black pixels
+  @position
+  A=M // Set pixel to black
+  M=1
+
+  @position // Move position forward
+  M=M+1
+
+  @LOOP // Return to loop
+  0;JMP
+
+  @END
+(END)
+ 0;JMP
